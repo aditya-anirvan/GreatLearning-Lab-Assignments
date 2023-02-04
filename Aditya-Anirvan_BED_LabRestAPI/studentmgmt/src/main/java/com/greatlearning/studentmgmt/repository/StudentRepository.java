9aeb2f05344ab5a89e7b3ae13a074d61b901b339
Aditya Anirvan <62769594+aditya-anirvan@users.noreply.github.com>
2023-02-05 04:10:10 +0530

    Delete Aditya-Anirvan_BED_LabRestAPI/studentmgmt directory

--- Aditya-Anirvan_BED_LabRestAPI/studentmgmt/src/main/java/com/greatlearning/studentmgmt/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.greatlearning.studentmgmt.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.studentmgmt.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
