4432d7d5302cb9965af5b477a83ef66d31a67497
Aditya Anirvan <62769594+aditya-anirvan@users.noreply.github.com>
2023-02-05 04:07:51 +0530

    Added Lab 6

---
+++ Aditya-Anirvan_BED_LabRestAPI/studentmgmt/src/main/java/com/greatlearning/studentmgmt/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.greatlearning.studentmgmt.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.studentmgmt.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
